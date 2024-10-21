015613ec741604bb6f4a5c966331d9b46b00b58d
Davi <davisko@pm.me>
2024-10-21 18:33:09 -0300

    atualizando com resolucao do bug de conversao?

--- iris.R
+++ iris.R
@@ -1,6 +1,7 @@
 # novo script para o cap 4
 
-dados = read.table('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv', sep =',', header = T)
+dados = read.table('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv', sep =',', header = T, stringsAsFactors = TRUE)
+
 
 # tipo de objeto
 class(dados)
